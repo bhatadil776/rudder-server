625640cb8cd8a1538077538e2d1170b0f91ecc32
Francesco Casula <fra.casula@gmail.com>
2023-06-09 13:33:02 +0200

    chore: gwconsumer

--- cmd/gwconsumer/main.go
+++ cmd/gwconsumer/main.go
@@ -8,6 +8,8 @@ import (
 	"os/signal"
 	"strconv"
 
+	"golang.org/x/sync/errgroup"
+
 	"github.com/apache/pulsar-client-go/pulsar"
 )
 
@@ -15,13 +17,19 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
 	defer cancel()
 
+	// Get the number of nodes from the environment variable
+	noOfNodes, ok := readIntEnvVar("NO_OF_NODES")
+	if !ok {
+		log.Fatalf("required environment variable is not set or is invalid: NO_OF_NODES")
+	}
+
 	pulsarURL := os.Getenv("PULSAR_URL")
 	topicName := os.Getenv("TOPIC_NAME")
 	maxConnectionsPerBroker := os.Getenv("MAX_CONNECTIONS_PER_BROKER")
 
 	log.Printf(
-		"Starting with configuration: Pulsar URL: %s, Topic: %s, MaxConnsPerBroker: %s\n",
-		pulsarURL, topicName, maxConnectionsPerBroker,
+		"Starting with configuration: Pulsar URL: %s, Topic: %s, MaxConnsPerBroker: %s, Nodes: %d\n",
+		pulsarURL, topicName, maxConnectionsPerBroker, noOfNodes,
 	)
 
 	maxConnectionsPerBrokerInt, err := strconv.Atoi(maxConnectionsPerBroker)
@@ -33,6 +41,7 @@ func main() {
 		ctx,
 		pulsarURL, topicName,
 		maxConnectionsPerBrokerInt,
+		noOfNodes,
 	)
 	if err != nil {
 		log.Fatal("Server error:", err)
@@ -41,7 +50,7 @@ func main() {
 
 func run(
 	ctx context.Context, pulsarURL, topicName string,
-	maxConnectionsPerBroker int,
+	maxConnectionsPerBroker, noOfNodes int,
 ) error {
 	client, err := pulsar.NewClient(pulsar.ClientOptions{
 		URL:                     pulsarURL,
@@ -52,22 +61,46 @@ func run(
 	}
 	defer client.Close()
 
-	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
-		Topic:            topicName,
-		SubscriptionName: "gwconsumer",
-		Type:             pulsar.KeyShared,
-	})
+	group, groupCtx := errgroup.WithContext(ctx)
+
+	for i := 0; i < noOfNodes; i++ {
+		i := i
+		group.Go(func() error {
+			instanceTopic := topicName + "-" + strconv.Itoa(i)
+			consumer, err := client.Subscribe(pulsar.ConsumerOptions{
+				Topic:            instanceTopic,
+				SubscriptionName: "gwconsumer",
+				Type:             pulsar.KeyShared,
+			})
+			if err != nil {
+				return fmt.Errorf("failed to subscribe to topic %s: %v", instanceTopic, err)
+			}
+			defer consumer.Close()
+
+			for {
+				msg, err := consumer.Receive(groupCtx)
+				if err != nil {
+					return fmt.Errorf("consumer receive error: %v", err)
+				}
+
+				log.Printf("Got message from writeKey %q: %s", msg.Key(), msg.Payload())
+			}
+		})
+	}
+
+	return group.Wait()
+}
+
+func readIntEnvVar(name string) (int, bool) {
+	rawValue, exists := os.LookupEnv(name)
+	if !exists {
+		return 0, false
+	}
+
+	value, err := strconv.Atoi(rawValue)
 	if err != nil {
-		return fmt.Errorf("failed to subscribe to topic: %v", err)
+		return 0, false
 	}
-	defer consumer.Close()
 
-	for {
-		msg, err := consumer.Receive(ctx)
-		if err != nil {
-			return fmt.Errorf("consumer receive error: %v", err)
-		}
-
-		log.Printf("Got message from writeKey %q: %s", msg.Key(), msg.Payload())
-	}
+	return value, true
 }
